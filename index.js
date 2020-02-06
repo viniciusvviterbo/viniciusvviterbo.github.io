1685868b67ef004ff9beeb5cec2c00365ed73bcb
Vinícius Verício Viterbo <PUCMINAS\1117988@pmg34infll20525.pucminas.net>
2020-02-06 15:34:22 -0300

    Alterações do CSS da página para teste relativo à responsividade

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-var Typer={
+var Typer = {
 	text: null,
-	accessCountimer:null,
-	index:0, 
-	speed:2,
-	file:"", 
-	accessCount:0,
-	deniedCount:0, 
-	init: function(){
-		accessCountimer=setInterval(function(){Typer.updLstChr();},500); 
-		$.get(Typer.file,function(data){
-			Typer.text=data;
-			Typer.text = Typer.text.slice(0, Typer.text.length-1);
+	accessCountimer: null,
+	index: 0, 
+	speed: 2,
+	file: "", 
+	accessCount: 0,
+	deniedCount: 0,
+	init: function() {
+		accessCountimer = setInterval(function() {Typer.updLstChr();}, 500); 
+		$.get(Typer.file,function(data) {
+			Typer.text = data;
+			Typer.text = Typer.text.slice(0, Typer.text.length - 1);
 		});
 	},
- 
-	content:function(){
+	content: function() {
 		return $("#console").html();
 	},
- 
-	write:function(str){
+	write: function(str) {
 		$("#console").append(str);
 		return false;
 	},
- 
-	addText:function(key){
-		
-		if(key.keyCode==18){
+	addText: function(key) {
+		if(key.keyCode==18) {
 			Typer.accessCount++; 
 			
-			if(Typer.accessCount>=3){
+			if(Typer.accessCount >= 3) {
 				Typer.makeAccess(); 
 			}
 		}
 		
-    		else if(key.keyCode==20){
+		else if(key.keyCode == 20) {
 			Typer.deniedCount++; 
 			
 			if(Typer.deniedCount>=3){
@@ -41,42 +37,41 @@ var Typer={
 			}
 		}
 		
-    		else if(key.keyCode==27){ 
-			Typer.hidepop(); 
+		else if(key.keyCode==27) {
+			Typer.hidepop();
 		}
 		
-    		else if(Typer.text){ 
-			var cont=Typer.content(); 
-			if(cont.substring(cont.length-1,cont.length)=="|") 
-				$("#console").html($("#console").html().substring(0,cont.length-1)); 
-			if(key.keyCode!=8){ 
-				Typer.index+=Typer.speed;	
-			}
-      		else {
-			if(Typer.index>0) 
-				Typer.index-=Typer.speed;
-			}
-			var text=Typer.text.substring(0,Typer.index)
-			var rtn= new RegExp("\n", "g"); 
+		else if(Typer.text) { 
+			var cont = Typer.content(); 
+			
+			if(cont.substring(cont.length - 1, cont.length) == "|")
+				$("#console").html($("#console").html().substring(0, cont.length - 1)); 
+			
+			if(key.keyCode != 8)
+				Typer.index += Typer.speed;	
+			
+      		else
+				if(Typer.index > 0) 
+					Typer.index -= Typer.speed;
+			
+			var text = Typer.text.substring(0, Typer.index)
+			var rtn = new RegExp("\n", "g"); 
 	
-			$("#console").html(text.replace(rtn,"<br/>"));
-			window.scrollBy(0,50); 
+			$("#console").html(text.replace(rtn, "<br/>"));
+			window.scrollBy(0, 50);
 		}
 		
-		if (key.preventDefault && key.keyCode != 122) { 
-			key.preventDefault()
-		};  
+		if (key.preventDefault && key.keyCode != 122)
+			key.preventDefault();
 		
-		if(key.keyCode != 122){ // otherway prevent keys default behavior
+		if(key.keyCode != 122) // otherway prevent keys default behavior
 			key.returnValue = false;
-		}
 	},
- 
-	updLstChr:function(){ 
-		var cont=this.content(); 
+	updLstChr:function() {
+		var cont = this.content(); 
 		
-		if(cont.substring(cont.length-1,cont.length)=="|") 
-			$("#console").html($("#console").html().substring(0,cont.length-1)); 
+		if(cont.substring(cont.length-1, cont.length) == "|") 
+			$("#console").html($("#console").html().substring(0, cont.length - 1)); 
 		
 		else
 			this.write("|"); // else write it
@@ -92,13 +87,11 @@ function replaceUrls(text) {
 		return text.replace(url, "<a href=\""  + url + "\">" + url + "</a>");
 	}
 
-	else {
-		return text
-	}
+	else return text
 }
 
-Typer.speed=3;
-Typer.file="viniciusvviterbo.txt"; // add your own name here
+Typer.speed = 3;
+Typer.file = "viniciusvviterbo.txt";
 Typer.init();
 
 var timer = setInterval("t();", 30);
