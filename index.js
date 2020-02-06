1ab1f870bb465a3113d8195d442d1dc55287d9fe
Vinícius Verício Viterbo <PUCMINAS\1117988@pmg34infll20525.pucminas.net>
2020-02-06 08:31:17 -0300

    Adição de conteúdo inicial

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed=3;
-Typer.file="[your-name].txt"; // add your own name here
+Typer.file="viniciusvviterbo.txt"; // add your own name here
 Typer.init();
 
 var timer = setInterval("t();", 30);
